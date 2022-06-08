bcb51a4d11807106ccdc596cbf4a8f392b8577aa
John McMahon <trakeen@gmail.com>
2022-06-08 12:33:44 -0400

    added progromatic model to TableEntiity conversion

--- FECIngest/FECIngest.cs
+++ FECIngest/FECIngest.cs
@@ -1,34 +1,47 @@
-using System;
+using Azure.Data.Tables;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using Microsoft.Azure;
+using System;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace FECIngest
 {
     public class GetFECData
     {
-        const string apiKey = "xT2E5C0eUKvhVY74ylbGf4NWXz57XlxTkWV9pOwu";
+        private const string apiKey = "xT2E5C0eUKvhVY74ylbGf4NWXz57XlxTkWV9pOwu";
+
         [FunctionName("FECIngest")]
-        public static async Task Run([TimerTrigger("0 */2 * * * *")]TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("0 */2 * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
             //find all candidates for MD
-            CandidateSearcher mdCandidates =new CandidateSearcher(apiKey,"MD");
+            CandidateSearcher mdCandidates = new CandidateSearcher(apiKey, "MD");
             await mdCandidates.Submit();
             log.LogInformation("Found {1} candidates.", mdCandidates.Candidates.Count);
-
             //save candidate data to table storage
+            TableClient tableClient = new TableClient("UseDevelopmentStorage=true", "MDWatchDEV");
 
+            foreach (var candidate in mdCandidates.Candidates)
+            {
+                //candidate.ToTableEntity
+                var fixedCandidate = candidate.AddUTC();
+
+                var status = await tableClient.AddEntityAsync(fixedCandidate.ToTable(tableClient, "Candidate", candidate.CandidateId));
+                if (status.IsError)
+                {
+                    log.LogInformation("Problem writing candidate to table:{1}", candidate.Name);
+                }
+            }
+
+            /*
             //get committee data
-            CommitteeSearcher committeeInfo = new CommitteeSearcher(apiKey);
+            //CommitteeSearcher committeeInfo = new CommitteeSearcher(apiKey);
             //committeeInfo.SetCandidate(mdCandidates.Candidates[1].CandidateId);
-            
+
             //need to batch requests to reduce calls, use queue or rate limiting to not exceed 1000 requests per hour
             //await committeeInfo.Submit();
-            
+
             foreach (var candidate in mdCandidates.Candidates)
             {
                 //log.LogInformation(candidate.CandidateId);
@@ -37,13 +50,11 @@ namespace FECIngest
                     committeeInfo.SetCandidate(candidate.CandidateId);
                     await committeeInfo.Submit();
                 }
-                
-                
-                
+
                 //log.LogInformation(committeeInfo.Committees.Count.ToString());
             }
             log.LogInformation(committeeInfo.Committees.Count.ToString());
-
+            */
         }
     }
 }
