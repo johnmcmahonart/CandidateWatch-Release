8c41b4073c49cb6d25326de2a109481350540600
John McMahon <john@johnmcmahonart.com>
2022-10-14 11:30:18 -0400

    terraform
    finished deployment script

--- terraform/solutionDeployV1.tf
+++ terraform/solutionDeployV1.tf
@@ -11,12 +11,13 @@ variable "rg_name" {
 variable "solution_prefix" {}
 variable "keyvault" {}
 variable "fec_access_key"{}
-variable "committeeprocessQueue"{}
-variable "financetotalsQueue" {}
-variable "schedulebdetailprocessQueue" {}
-variable "schedulebpageprocessQueue"{}
+variable "table_affix" {}
+variable "process_queues" {}
+variable "functions" {}
+variable "state_twoletter" {}
 
-
+data azurerm_client_config "current" { }
+data azurerm_subscription "current" {}
 resource "azurerm_resource_group" "CandidateWatchRG" {
   name     = var.rg_name
   location = "eastus"
@@ -31,6 +32,7 @@ resource "azurerm_storage_account" "SolutionStorageAccount" {
   account_tier = "Standard"
 }
 
+### USER AND ROLE DEFINITIONS
 #managed identity for static site to use to access storage for the solution
 resource "azurerm_user_assigned_identity" "solution_worker" {
   location            = azurerm_resource_group.CandidateWatchRG.location
@@ -38,41 +40,74 @@ resource "azurerm_user_assigned_identity" "solution_worker" {
   resource_group_name = azurerm_resource_group.CandidateWatchRG.name
 }
 
+#managed identity APIM
+resource "azurerm_user_assigned_identity" "apim_worker" {
+  location            = azurerm_resource_group.CandidateWatchRG.location
+  name                = "apim_worker"
+  resource_group_name = azurerm_resource_group.CandidateWatchRG.name
+}
+
 #roles necessary for data storage access
-resource "azurerm_role_assignment" "tableaccess" {
-  scope              = azurerm_resource_group.CandidateWatchRG.name
+resource "azurerm_role_assignment" "tableaccessfunctions" {
+  scope              = data.azurerm_subscription.current.id
   role_definition_name = "Storage Table Data Contributor"
   principal_id       = azurerm_user_assigned_identity.solution_worker.principal_id
 }
-resource "azurerm_role_assignment" "queueaccess" {
-  scope              = azurerm_resource_group.CandidateWatchRG.name
+resource "azurerm_role_assignment" "storageaccountfunctions" {
+  scope              = data.azurerm_subscription.current.id
+  role_definition_name = "Storage Account Data Contributor"
+  principal_id       = azurerm_user_assigned_identity.solution_worker.principal_id
+}
+
+resource "azurerm_role_assignment" "queueaccessfunctions" {
+  scope              = data.azurerm_subscription.current.id
   role_definition_name = "Storage Queue Data Contributor"
   principal_id       = azurerm_user_assigned_identity.solution_worker.principal_id
 }
 
+resource "azurerm_role_assignment" "tableaccessapim" {
+  scope              = data.azurerm_subscription.current.id
+  role_definition_name = "Storage Table Data Reader"
+  principal_id       = azurerm_user_assigned_identity.apim_worker.principal_id
+}
 
-#default table for solution, more will be created later
-resource "azurerm_storage_table" "maryland" {
-  name = "MDTable"
+resource "azurerm_role_assignment" "storageaccountapim" {
+  scope              = data.azurerm_subscription.current.id
+  role_definition_name = "Storage Account Data Contributor"
+  principal_id       = azurerm_user_assigned_identity.apim_worker.principal_id
+}
+
+resource "azurerm_key_vault_access_policy" "candidatewatchaccess" {
+  key_vault_id = "/subscriptions/782918b5-c24a-4ef2-8042-1b90bf912ae3/resourceGroups/BatchRendering/providers/Microsoft.KeyVault/vaults/SecVaultPrimary"
+  tenant_id    = "${data.azurerm_client_config.current.tenant_id}"
+  object_id    = "${azurerm_user_assigned_identity.solution_worker.principal_id}"
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+}
+
+### END SECURITY
+
+#tables for solution
+resource "azurerm_storage_table" "defaulttables" {
+  for_each=toset(var.state_twoletter)
+
+  
+  name= "${each.key}${var.table_affix}"
+  
   storage_account_name = azurerm_storage_account.SolutionStorageAccount.name
   
 }
 
-#necessary queues for data ingestion process
+#queues for data ingestion process
 resource "azurerm_storage_queue" "committeeprocess" {
-  name = "${var.committeeprocessQueue}"
-  storage_account_name = azurerm_storage_account.SolutionStorageAccount.name
-}
-resource "azurerm_storage_queue" "financetotalsprocess" {
-  name = "${var.financetotalsQueue}"
-  storage_account_name = azurerm_storage_account.SolutionStorageAccount.name
-}
-resource "azurerm_storage_queue" "schedulebdetailprocessQueue" {
-  name = "${var.schedulebdetailprocessQueue}"
-  storage_account_name = azurerm_storage_account.SolutionStorageAccount.name
-}
-resource "azurerm_storage_queue" "schedulebpageprocess" {
-  name = "${var.schedulebpageprocessQueue}"
+ for_each = toset(var.process_queues)
+  name = each.key
   storage_account_name = azurerm_storage_account.SolutionStorageAccount.name
 }
 
@@ -83,7 +118,7 @@ resource "azurerm_app_configuration" "solutionConf" {
   location = azurerm_resource_group.CandidateWatchRG.location
 }
 resource "azurerm_api_management" "restapi" {
-  name                = "conf${var.solution_prefix}apim"
+  name                = "${var.solution_prefix}apim"
   location            = azurerm_resource_group.CandidateWatchRG.location
   resource_group_name = azurerm_resource_group.CandidateWatchRG.name
   publisher_name      = "John McMahon"
@@ -98,7 +133,7 @@ resource "azurerm_api_management" "restapi" {
   location            = azurerm_resource_group.CandidateWatchRG.location
   identity {
     type = "UserAssigned"
-    identity_ids = azurerm_user_assigned_identity.solution_worker.id
+    identity_ids = ["${azurerm_user_assigned_identity.solution_worker.id}"]
   }
 
 }
@@ -110,14 +145,30 @@ resource "azurerm_service_plan" "appserviceplan" {
   sku_name            = "B1"
 }
 
-resource "azurerm_windows_function_app" "example" {
-  name                = "example-windows-function-app"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_windows_function_app" "functionworkers" {
+  for_each = toset(var.functions)
+  name = each.key
+  resource_group_name = azurerm_resource_group.CandidateWatchRG.name
+  location            = azurerm_resource_group.CandidateWatchRG.location
 
-  storage_account_name       = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  storage_account_name       = azurerm_storage_account.SolutionStorageAccount.name
+  storage_uses_managed_identity = true
   service_plan_id            = azurerm_service_plan.appserviceplan.id
+identity {
+    type = "UserAssigned"
+    identity_ids = ["${azurerm_user_assigned_identity.solution_worker.id}"]
+  }
 
   site_config {}
+}
+
+resource "azurerm_public_ip" "frontendip" {
+  name                = "frontendip"
+  resource_group_name = azurerm_resource_group.CandidateWatchRG.name
+  location            = azurerm_resource_group.CandidateWatchRG.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Production"
+  }
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,5 +1,8 @@
 solution_prefix="candidatewatch"
+table_affix="Table"
 keyvault="SecVaultPrimary"
 fec_access_key="FECAPISecret"
-
+process_queues=["committeeprocess","financetotalsprocess","schedulebcandidateprocess","schedulebpageprocess"]
+functions=["BuildCandidatebyYearPartition","GetCandidateData","GetFinanceData","GetCommitteeData","GetScheduleBDisbursement","PurgeAllSolutionTables","SendUpdates","Tests","ValidateScheduleB"]
+state_twoletter=["AL","KY","OH","AK","LA","OK","AZ","ME","OR","AR","MD","PA","AS","MA","PR","CA","MI","RI","CO","MN","SC","CT","MS","SD","DE","MO","TN","DC","MT","TX","FL","NE","TT","GA","NV","UT","GU","NH","VT","HI","NJ","VA","ID","NM","VI","IL","NY","WA","IN","NC","WV","IA","ND","WI","KS","CM","WY"]
 
