0280093b050c4bc4e0a63dfd97ff4cb589d85dd4
John McMahon <john@johnmcmahonart.com>
2022-10-31 15:28:52 -0400

    changes for api deployment
    revert scheduleb candidatewatch controller to use custom binder, add rest api definition to terraform, update terraform deployment script

--- RESTApi/Controllers/ScheduleBDetailController.cs
+++ RESTApi/Controllers/ScheduleBDetailController.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
-using AutoMapper;
+﻿using AutoMapper;
 using MDWatch.Model;
 using Microsoft.AspNetCore.Mvc;
 using RESTApi.DTOs;
 using RESTApi.Mapper;
 using RESTApi.Repositories;
-using Newtonsoft.Json.Linq;
 
 //using System.Web.Http;
 
@@ -29,42 +27,36 @@ namespace RESTApi.Controllers
         }
 
         [HttpGet("{year}/keys")]
-        public async Task<IEnumerable<IEnumerable<ScheduleBDetailDTO>>> GetbyKeysandElectionYearAsync([FromQuery] String keys, int year)
+        public async Task<IEnumerable<IEnumerable<ScheduleBDetailDTO>>> GetbyKeysandElectionYearAsync([StringArrayBinder] List<string> keys, int year)
 
         {
-            List<string> keysResult = keys.Split(',').ToList();
-
-            IEnumerable<IEnumerable<ScheduleBByRecipientID>> modelOut = await _scheduleBDetailRepository.GetbyKeysandElectionYearAsync(keysResult, year);
+            IEnumerable<IEnumerable<ScheduleBByRecipientID>> modelOut = await _scheduleBDetailRepository.GetbyKeysandElectionYearAsync(keys, year);
             List<List<ScheduleBDetailDTO>> outlist = new();
             return _mapper.Map<IEnumerable<IEnumerable<ScheduleBByRecipientID>>, IEnumerable<IEnumerable<ScheduleBDetailDTO>>>(modelOut);
             
         }
         [HttpGet("keys")]
-        public async Task<IEnumerable<IEnumerable<ScheduleBDetailDTO>>> GetbyKeysAsync(string keys)
+        public async Task<IEnumerable<IEnumerable<ScheduleBDetailDTO>>> GetbyKeysAsync([StringArrayBinder] List<string> keys)
 
         {
-            List<string> keysResult = keys.Split(',').ToList();
-            IEnumerable<IEnumerable<ScheduleBByRecipientID>> modelOut = await _scheduleBDetailRepository.GetbyKeysAsync(keysResult);
+            IEnumerable<IEnumerable<ScheduleBByRecipientID>> modelOut = await _scheduleBDetailRepository.GetbyKeysAsync(keys);
             return _mapper.Map<IEnumerable<IEnumerable<ScheduleBByRecipientID>>, IEnumerable<IEnumerable<ScheduleBDetailDTO>>>(modelOut);
             
         }
 
         [HttpGet("{key}/years/")]
-    public async Task<IEnumerable<ScheduleBDetailDTO>> GetbyCandidateandElectionYearsAsync([FromQuery] string years, string key)
+    public async Task<IEnumerable<ScheduleBDetailDTO>> GetbyCandidateandElectionYearsAsync([FromQuery] List<int> years, string key)
     {
-            List<int> yearsResult = years.Split(',').Select(int.Parse).ToList();
-            
-            IEnumerable<ScheduleBByRecipientID> modelOut = await _scheduleBDetailRepository.GetbyCandidateandElectionYearsAsync(yearsResult, key);
+        IEnumerable<ScheduleBByRecipientID> modelOut = await _scheduleBDetailRepository.GetbyCandidateandElectionYearsAsync(years, key);
             return _mapper.Map<IEnumerable<ScheduleBByRecipientID>, IEnumerable<ScheduleBDetailDTO>>(modelOut);
             
 
     }
 
     [HttpGet("years")]
-    public async Task<IEnumerable<ScheduleBDetailDTO>> GetbyElectionYearAsync([FromQuery] string years)
+    public async Task<IEnumerable<ScheduleBDetailDTO>> GetbyElectionYearAsync([FromQuery] List<int> years)
     {
-            List<int> yearsResult = years.Split(',').Select(int.Parse).ToList();
-            IEnumerable<ScheduleBByRecipientID> modelOut = await _scheduleBDetailRepository.GetbyElectionYearsAsync(yearsResult);
+        IEnumerable<ScheduleBByRecipientID> modelOut = await _scheduleBDetailRepository.GetbyElectionYearsAsync(years);
             return _mapper.Map<IEnumerable<ScheduleBByRecipientID>, IEnumerable<ScheduleBDetailDTO>>(modelOut);
             
     }
